48c01f1fdc7400b5157604ac3ec404de46e46101
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-24 00:41:12 +0300

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     index = 0
-    while index < x:
+    for i in range(x):
         try:
             print("{}".format(my_list[index]), end="")
             index += 1
