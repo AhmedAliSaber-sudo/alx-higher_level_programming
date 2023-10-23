65a00f7b2a21d847ca6c46603135c90d02ac090b
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-23 21:51:56 +0300

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 result = ""
 for number in range(0, 100):
-    if number != 100:
-        if number < 10:
-            result += "0{:d}, ".format(number)
-        else:
-            result += "{:d}, ".format(number)
-        if number == 99:
-            result = result.rstrip(', ')
+
+    if number < 10:
+        result += "0{:d}, ".format(number)
+    else:
+        result += "{:d}, ".format(number)
+    if number == 99:
+        result = result.rstrip(', ')
 print(result)
