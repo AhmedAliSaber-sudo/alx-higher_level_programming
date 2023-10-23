caf4aa8d288bd3dc3cc33df25acaf6d0f3217773
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-23 21:42:50 +0300

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    if ch == 113 or ch == 101:
-        continue
-    else:
+    if ch != 113 or ch != 101:
         print("{}".format(chr(ch)), end='')
+    else:
+        continue
