82464c29230df42b24a71e8a822d616d436054a9
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-23 22:36:18 +0300

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 for i in range(0, 26):
     ch = ord('z') - i
     if i % 2 == 1:
-        ch = (chr(c - ord('a') + ord('A')))
+        ch = (chr(ch - ord('a') + ord('A')))
     else:
         ch = chr(ch)
 
