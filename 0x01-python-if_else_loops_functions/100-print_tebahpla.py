87cba8a019457b25f194c71ee4a341699e7d6908
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-23 22:47:55 +0300

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(25, -1, -1):
     if n % 2 == 0:
-        print("{}".format(chr(i+65)), end="")
+        print("{}".format(chr(n+65)), end="")
     else:
-        print("{}".format(chr(i + 97)), end="")
+        print("{}".format(chr(n + 97)), end="")
