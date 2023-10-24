73e698e29f10c5e17513703aaa429a214d112e16
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-24 08:00:22 +0300

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    ch = sentence[0] if len(sentence) > 0 else None
 
-    length = len(sentence)
-    char = sentence[0] if length > 0 else None
-
-    return (length, char)
+    return (len(sentence), ch)
