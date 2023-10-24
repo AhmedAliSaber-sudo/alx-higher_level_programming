7f4a2ba6ab808d23d7746667c82f6465fc6deabe
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-25 02:20:54 +0300

    update

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 Module 102-square
-Defines class Square with private size and public area
-Can access and update size
 """
 
 
@@ -10,72 +8,39 @@ class Square:
     """
     class Square definition
 
-    Args:
-        size (int): size of a side in square
-
-    Functions:
-        __init__(self, size)
-        size(self)
-        size(self, value)
-        area(self)
     """
 
     def __init__(self, size=0):
         """
         Initializes square
 
-        Attributes:
+        Args:
             size (int): defaults to 0 if none; don't use __size to call setter
         """
         self.size = size
 
-    @property
-    def size(self):
-        """"
-        Getter
-
-        Return: size
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        Setter
-
-        Args:
-            value: sets size to value, if int and >= 0
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
     def area(self):
         """
         Calculates area of square
-        Returns:
-            area
+        Returns the area
         """
         return (self.__size)**2
 
     def __eq__(self, other):
         """
-        Compares and returns if equal
+        Compares if equal and returns the result
         """
         return self.size == other.size
 
     def __ne__(self, other):
         """
-        Compares and returns if not equal
+        Compares if not equal and returns the result
         """
         return self.size != other.size
 
     def __lt__(self, other):
         """
-        Compares and returns if lesser than
+        Compares if lesser than and returns the result
         """
         return self.size < other.size
 
@@ -96,3 +61,24 @@ class Square:
         Compares and returns if greater than or equal to
         """
         return self.size >= other.size
+
+    @property
+    def size(self):
+        """"
+        Get
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
