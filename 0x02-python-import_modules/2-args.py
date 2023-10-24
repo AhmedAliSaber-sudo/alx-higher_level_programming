ef9cade172a4c25cd655672b982ad797637d4a36
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-24 04:21:39 +0300

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(size))
     for index in range(size):
-        print("{}: {}".format(index + 1, sys.argv[index + 1]))
+        print("{}: {}".format(index + 1, argv[index + 1]))
