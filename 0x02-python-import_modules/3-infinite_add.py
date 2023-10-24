48d3bb6030b3d06808fd0cc5ae6dadd0bcafdf4e
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-24 04:24:17 +0300

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     res = 0
     if (len(sys.argv) > 1):
-        for i in range(len(sys.argv)):
+        for i in range(len(1, sys.argv)):
             res += (int(sys.argv[i]))
     print("{:d}".format(res))
