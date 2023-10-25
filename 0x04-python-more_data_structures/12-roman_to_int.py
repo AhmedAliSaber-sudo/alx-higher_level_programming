014ba5881fbc118c88c65ef71473bfdc816a9884
Ahmed Ali <ahmedewida221510@gmail.com>
2023-10-25 07:17:27 +0300

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+
+    values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = 0
+
     if roman_string is None or type(roman_string) is not str:
         return 0
     if roman_string == "":
         return 0
-    num = 0
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
     for i, j in zip(roman_string, roman_string[1:]):
-        if i not in dic.keys():
+        if i not in values.keys():
             return 0
-        elif dic[i] >= dic[j]:
-            num += dic[i]
+        elif values[i] >= values[j]:
+            number += values[i]
         else:
-            num -= dic[i]
-    num += dic[roman_string[-1]]
-    return num
+            number -= values[i]
+    number += values[roman_string[-1]]
+    return number
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,4 +2,3 @@
 def number_keys(a_dictionary):
     if a_dictionary is not None:
         return(len(a_dictionary.keys()))
-    return(0)
